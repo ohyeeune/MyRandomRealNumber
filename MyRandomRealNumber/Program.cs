6ef4a75079e52be263baefa6568bf5e33b0e0933
ohyeeune <s2018s33@e-mirim.hs.kr>
2020-05-19 18:03:32 +0900

    임의의 실수를 사용해서 1~200 사이 숫자 맞추기~

--- MyRandomRealNumber/Program.cs
+++ MyRandomRealNumber/Program.cs
@@ -10,6 +10,30 @@ namespace MyRandomRealNumber
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int answer = random.Next(1, 200);
+            //1에서 200중에 랜덤값을 1개
+            //Console.WriteLine(answer);
+
+            while(true)
+            {
+                Console.Write("숫자를 입력해주세요 : ");
+                int input = int.Parse(Console.ReadLine());
+
+                if(input > answer)
+                {
+                    Console.WriteLine(input + "보다는 작은 숫자입니다.");
+                    Console.WriteLine();
+                } else if(input < answer)
+                {
+                    Console.WriteLine(input + "보다는 큰 숫자입니다.");
+                    Console.WriteLine();
+                } else if(input == answer)
+                {
+                    Console.WriteLine("정답입니다!");
+                    break;
+                }
+            }
         }
     }
 }
